303b56be7b8e0164ab743694ce2978030f886b10
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-06-01 21:08:56 -0300

    Create React App

---
+++ configs/install-node/install-node-ubuntu-20.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo snap install code --classic
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install gcc g++ make
