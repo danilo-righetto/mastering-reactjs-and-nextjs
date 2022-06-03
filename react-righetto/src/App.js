2069bd72782ca18c051878b1e480e37638aad480
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-06-03 09:01:18 -0300

    Component StateFull

--- react-righetto/src/App.js
+++ react-righetto/src/App.js
@@ -3,15 +3,48 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     name: 'Danilo Righetto',
+  //     age: 28
+  //   };
+
+  //   // Realizando o bind do React para obter o estado
+  //   // this.handlePClick = this.handlePClick.bind(this);
+  // }
+
+  // Usando userFields
+  state = {
+    name: 'Danilo Righetto',
+    age: 28
+  };
+
+  handlePClick = () => {
+    const { name } = this.state;
+    console.log(`<p></p> click ${name}`);
+    this.setState({ name: 'Ana' });
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { age } = this.state;
+    console.log(age);
+    this.setState({ age: age + 1 });
+  }
+
   render() {
+    const { name, age } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            Name: {name} - Age: {age}
           </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
