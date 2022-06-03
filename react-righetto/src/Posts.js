b9b1962f2d1c329925a13276dc9421a5216494d1
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-06-03 12:00:37 -0300

    State with arrays and objects

---
+++ react-righetto/src/Posts.js
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+
+class Posts extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo 1',
+        body: 'O body 1'
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O body 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O body 3'
+      }
+    ]
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="App">
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    )
+  };
+}
+
+export default Posts;
