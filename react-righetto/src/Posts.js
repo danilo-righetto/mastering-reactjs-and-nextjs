40b69df3e8ffc8e070b2572e02ed97b406586696
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-06-05 22:28:48 -0300

    Lifecycle Methods

--- react-righetto/src/Posts.js
+++ react-righetto/src/Posts.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 class Posts extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -17,14 +18,43 @@ class Posts extends Component {
         id: 3,
         title: 'O titulo 3',
         body: 'O body 3'
+      },
+      {
+        id: 4,
+        title: 'O titulo 4',
+        body: 'O body 4'
       }
     ]
   };
 
+  timeOutUpdate = null;
+
+  componentDidMount() {
+    console.log('Oi');
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O titulo 1 novo';
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 5000);
+  }
+
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
     return (
       <div className="App">
+        <h1>Contador: {counter}</h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
