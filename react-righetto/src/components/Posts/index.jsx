de0722ee4e9651b042a83a2cdaf9f3c5f5a214b5
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-06-06 20:17:42 -0300

    Organization of components

---
+++ react-righetto/src/components/Posts/index.jsx
@@ -0,0 +1,15 @@
+import { PostCard } from "../PostCard";
+export const Posts = ({ posts }) => {
+  return (<div className="posts">
+    {posts.map((post) => (
+      <PostCard
+        title={post.title}
+        body={post.body}
+        cover={post.cover}
+        id={post.id}
+        key={post.id}
+      />
+    ))}
+  </div>
+  );
+};
